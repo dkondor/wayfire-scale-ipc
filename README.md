f46aca5cd21ad3184b26f8401f461a42d06b7276
Kondor Dániel <kondor.dani@gmail.com>
2023-09-30 05:21:03 +0200

    Add source and README

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # wayfire-scale-ipc
-Extra IPC interaction for the scale plugin of Wayfire
+
+Extra IPC interaction for the scale plugin of Wayfire. Currently, it provides the `scale_ipc_filter/activate_appid` IPC method, which will start scale and filter the displayed apps based on the `app_id` parameter given. This can be used by e.g. a taskbar or dock to show all open views of one app. An example implementation for Cairo-Dock is given [here](https://github.com/dkondor/cairo-dock-core/blob/wayfire_integration/src/implementations/cairo-dock-wayfire-integration.c).
+
+## Installing
+
+### 1. Dependencies
+
+It needs a recent version of Wayfire (at least commit [48c3048](https://github.com/WayfireWM/wayfire/pull/1864/commits/48c30481afe47c8235885d2a2c7378091e6293f2)) and [nlohmann_json/](https://github.com/nlohmann/json/). For building, it needs [Meson](https://mesonbuild.com/).
+
+
+### 2. Downloading the source
+
+Use git:
+
+```
+git clone https://github.com/dkondor/wayfire-scale-ipc.git
+cd wayfire-scale-ipc
+```
+
+or the "Download ZIP" option (click on the green "Code" button above).
+
+
+### 3. Building and installing
+
+Before using, the code needs to be compiled and installed. It uses the standard Meson way:
+
+```
+meson build
+ninja -C build
+sudo ninja -C build install
+```
+
+### 4. Usage
+
+Enable it in [WCM](https://github.com/WayfireWM/wcm) ("Scale IPC addon" in the Utilities category) or in the config file by adding `scale_ipc_filter` to the list of active plugins (`core/plugins`).
+
+Important: it needs the IPC plugin to also be active! This currently does not show up in WCM, but needs to be enabled in the config file:
+1. Open the config file in a text editor (by default, it is under `~/.config/wayfire.ini`)
+2. Find the `[core]` section and the line starting with `plugins = `
+3. Add `ipc` anywhere in this line
+
+Once this plugin is enabled, it can be activated by sending the `scale_ipc_filter/activate_appid` command via IPC. A simple example in Python that uses the [wayfire_socket](https://github.com/WayfireWM/wayfire/blob/master/ipc-scripts/wayfire_socket.py) imlementation would work as follows:
+
+```
+import wayfire_socket as ws
+socket = ws.WayfireSocket('/tmp/wayfire-wayland-1.socket')
+msg = ws.get_msg_template('scale_ipc_filter/activate_appid')
+msg['data']['all_workspaces'] = True
+msg['data']['app_id'] = 'sakura'
+socket.send_json(msg)
+```
+
+This should show all open instances of Sakura (terminal app) in scale. If everything works, the method call should return the following response:
+```
+{'result': 'ok'}
+```
+
+The following options are supported (add to the `data` dictionary of the message):
+ - `app_id`: string, application ID to filter for; only apps that match this exactly will be shown (required)
+ - `case_sensitive`: boolean, whether the app ID is matched in a case sensitive way (optional, default: True)
+ - `all_workspaces`: boolean, whether views from all workspaces will be included (optional, default: False)
+ - `output_id`: integer, numeric ID of the output (monitor) to use (optional, by default, the currently active output is used)
+
+Possible issues:
+ - Cannot connect to the socket (e.g. `FileNotFoundError` exception in the above code): IPC plugin is not activated (check in the config file), or Wayfire uses a different filename for sockets (check in `/tmp` or the `WAYFIRE_SOCKET` environment variable).
+ - Response is `{'error': 'No such method found!'}`: this plugin is not activated, check in WCM or in the config file.
+ - All views disappear: this can happen if `app_id` does not match any open view. In this case, simply exit scale with its activator keybinding.
+
+
+
---
+++ scale_ipc_filter.cpp
@@ -0,0 +1,116 @@
+#include <wayfire/core.hpp>
+#include <wayfire/util.hpp>
+#include <wayfire/output.hpp>
+#include <wayfire/plugin.hpp>
+#include <wayfire/per-output-plugin.hpp>
+#include <wayfire/toplevel-view.hpp>
+#include <wayfire/plugins/common/shared-core-data.hpp>
+#include <wayfire/plugins/scale-signal.hpp>
+#include <wayfire/plugins/ipc/ipc-method-repository.hpp>
+#include <wayfire/plugins/ipc/ipc-helpers.hpp>
+#include <nlohmann/json.hpp>
+
+class scale_ipc_activator : public wf::per_output_plugin_instance_t {
+	private:
+		
+		bool active = false;
+		std::string current_filter; /* currently active filter */
+		bool current_case_sensitive = true; /* whether the filter should be case sensitive */
+		
+		bool should_show_view(wayfire_toplevel_view view) const {
+			const std::string& app_id_str = current_filter;
+			if(app_id_str.empty()) return true;
+			
+			if(!current_case_sensitive) {
+				std::string app_id = view->get_app_id();
+				std::transform(app_id.begin(), app_id.end(), app_id.begin(),
+					[] (unsigned char c) { return (char)std::tolower(c); });
+				return app_id == app_id_str;
+			}
+			else return view->get_app_id() == app_id_str;
+		}
+		
+	public:
+		
+		void init() override {
+			output->connect(&view_filter);
+			output->connect(&scale_end);
+		}
+		
+		void fini() override {
+			view_filter.disconnect();
+			scale_end.disconnect();
+		}
+		
+		nlohmann::json activate(const std::string& filter, bool case_sensitive, bool all_workspaces) {
+			current_filter = filter;
+			current_case_sensitive = case_sensitive;
+			active = true;
+			if (!output->is_plugin_active("scale")) {
+				nlohmann::json data;
+				data["output_id"] = output->get_id();
+				wf::shared_data::ref_ptr_t<wf::ipc::method_repository_t> repo;
+				return repo->call_method(all_workspaces ? "scale/toggle_all" : "scale/toggle", data);
+			}
+			else {
+				scale_update_signal signal;
+				output->emit(&signal);
+				return wf::ipc::json_ok();
+			}
+		}
+
+		wf::signal::connection_t<scale_filter_signal> view_filter = [this] (scale_filter_signal *signal) {
+			if (active) scale_filter_views(signal, [this] (wayfire_toplevel_view v) {
+					return !should_show_view(v);
+				});
+		};
+		
+		wf::signal::connection_t<scale_end_signal> scale_end = [this] (scale_end_signal *data) {
+			active = false;
+		};
+};
+
+class scale_ipc_activator_global : public wf::plugin_interface_t,
+		public wf::per_output_tracker_mixin_t<scale_ipc_activator> {
+	private:
+		
+		wf::shared_data::ref_ptr_t<wf::ipc::method_repository_t> method_repository;
+	
+	public:
+	
+		void init() override {
+			this->init_output_tracking();
+			method_repository->register_method("scale_ipc_filter/activate_appid", activate);
+		}
+		
+		void fini() override {
+			method_repository->unregister_method("scale_ipc_filter/activate_appid");
+			this->fini_output_tracking();
+		}
+		
+		wf::ipc::method_callback activate = [=] (nlohmann::json data) {
+			bool case_sensitive  = true;
+			bool all_workspaces  = false;
+			wf::output_t *output = nullptr;
+			
+			WFJSON_EXPECT_FIELD(data, "app_id", string);
+			WFJSON_OPTIONAL_FIELD(data, "case_sensitive", boolean);
+			WFJSON_OPTIONAL_FIELD(data, "all_workspaces", boolean);
+			WFJSON_OPTIONAL_FIELD(data, "output_id", number_integer);
+			
+			if (data.count("case_sensitive")) case_sensitive = data["case_sensitive"];
+			if (data.count("all_workspaces")) all_workspaces = data["all_workspaces"];
+			if (data.count("output_id")) {
+				output = wf::ipc::find_output_by_id(data["id"]);
+				if (!output) return wf::ipc::json_error("output not found");
+			}
+			else {
+				output = wf::get_core().get_active_output();
+				if (!output) return wf::ipc::json_error("no active output");
+			}
+			
+			return this->output_instance[output]->activate(data["app_id"], case_sensitive, all_workspaces);
+		};
+};
+
+DECLARE_WAYFIRE_PLUGIN(scale_ipc_activator_global)
